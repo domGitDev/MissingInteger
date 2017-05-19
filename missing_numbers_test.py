de57ecb70e810f183c6ef6751800bf28b349d513
domGitDev <silvadom2014@gmail.com>
2017-05-19 15:11:59 +0000

    Update missing_numbers_test.py
    Unittest for Missing Numbers

--- missing_numbers_test.py
+++ missing_numbers_test.py
@@ -1,1 +1,31 @@
 
+import unittest
+from missing_numbers import find_missing
+
+
+class FindMissingTest(unittest.TestCase):
+	
+	def test_missing_pair(self):
+		m = 10
+		A = [203, 204, 205, 206, 207, 
+			208, 203, 204, 205, 206]
+		n = 13
+		B = [203, 204, 204, 205, 206, 207, 
+			205, 208, 203, 206, 205, 206, 204]
+			
+		self.assertEqual([204, 205, 206], find_missing(A, m, B, n))	
+		
+	def test_find_missing_type(self):
+		with self.assertRaises(TypeError):
+			find_missing('dg', 2, [0, 3], 2)
+			
+		with self.assertRaises(TypeError):
+			find_missing(10000, 1, 100, 1)
+			
+		with self.assertRaises(TypeError):
+			find_missing([6, 7, 8], 3, 'this', 4)		
+
+suite = unittest.TestLoader().loadTestsFromTestCase(FindMissingTest)
+unittest.TextTestRunner(verbosity=2).run(suite)
+
+
